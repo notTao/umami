62958316dca0fc738935be0016b1c7f9490094d8
Adaki <lumbreras.david.108@gmail.com>
2026-04-30 17:23:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Preloader - Umami
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    const body = document.body;
+    const loaderText = document.getElementById('loader-text');
 
+    loaderText.innerText = "¡Listo!";
+
+    setTimeout(() => {
+        preloader.style.opacity = '0';
+        
+        setTimeout(() => {
+            preloader.style.display = 'none';
+            body.classList.remove('loading');
+        }, 600);
+    }, 500); 
+});
+
+setTimeout(() => {
+    if (document.body.classList.contains('loading')) {
+        document.body.classList.remove('loading');
+        document.getElementById('preloader').style.display = 'none';
+    }
+}, 10000);
